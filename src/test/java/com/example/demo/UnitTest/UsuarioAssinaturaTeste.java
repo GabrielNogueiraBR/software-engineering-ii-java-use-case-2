231efc5bc32f98230f12f273e42cbf5990c9d3cf
Raul Ryan Deaque Silva <42502534+ryanraul@users.noreply.github.com>
2023-03-27 20:34:15 -0300

    Implementação dos casos de teste

--- src/test/java/com/example/demo/UnitTest/UsuarioAssinaturaTeste.java
+++ src/test/java/com/example/demo/UnitTest/UsuarioAssinaturaTeste.java
@@ -22,5 +22,50 @@ public class UsuarioAssinaturaTeste {
 		// Resultados
 		assertEquals(UserType.ASSINANTE,joao.getTipo());
 	}
+
+	//BDD - Raul
+	@Test
+	public void deveNaoRealizarAssinaturaCasoSaldoInsuficiente(){
+		// Cenários
+		School escolaOnline = new School("Escola de Programação do Deaque");
+		Plan planoBasico = new Plan("Plano básico de programação", UserType.ASSINANTE, 200);	
+		User joao = new User("João Perinpinpin", "jperinpinpin@gpin.com", "senha123");
+		UserType atualTipoUsuario = joao.getTipo();
+
+		// Execução
+		escolaOnline.adicionaPlano(planoBasico);
+		joao.adicionarSaldo(30);
+		
+		String mensagemRetorno = joao.assinaPlano(planoBasico);
+		
+		// Resultados
+		assertEquals(atualTipoUsuario, joao.getTipo());
+		assertEquals("Usuário não tem saldo suficiente para assinatura do plano.", mensagemRetorno);
+		assertEquals(30, joao.getSaldo());
+	}
 	
+	//BDD - Ferraz
+
+	@Test
+	public void deveBloquearCasoSejaTentativaDuplicada() {
+
+		// Cenários
+		School escolaOnline = new School("Escola de Programação do Deaque");
+		Plan planoBasico = new Plan("Plano básico de programação", UserType.ASSINANTE, 23.90);	
+		User ze = new User("Zezin", "ze@ze.com", "senha123");
+		joao.adicionarSaldo(50);
+		double saldoAtualUsuario = joao.getSaldo();
+		String mensagemRetorno = "";
+		
+		// Execução
+		escolaOnline.adicionaPlano(planoBasico);
+		mensagemRetorno = joao.assinaPlano(planoBasico);
+		
+		escolaOnline.adicionaPlano(planoBasico);
+		mensagemRetorno = joao.assinaPlano(planoBasico);
+
+		// Resultados
+		assertEquals("Usuário já pagou o boleto desde mês, favor aguardar a proxima parcela.", mensagemRetorno);
+		assertEquals(saldoAtualUsuario - planoBasico.Valor, joao.getSaldo());
+	}
 }
